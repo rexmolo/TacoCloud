a1b45c006c31615bc977fe988e66f9c6f18d975e
rexmolo <jackie.hui.ma@gmail.com>
2024-02-12 21:12:53 +0100

    tacoOrder class

---
+++ src/main/java/io/github/rexmolo/tacocloud/TacoOrder.java
@@ -0,0 +1,23 @@
+package io.github.rexmolo.tacocloud;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TacoOrder {
+
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addTaco(Taco taco) {
+        tacos.add(taco);
+    }
+
+}
